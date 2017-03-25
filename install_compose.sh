4a7923a0f72cf3ff9b6113f8507913ae33a196bf
frjaraur <frjaraur@gmail.com>
2017-03-25 13:23:37 +0100

    Changed docker-compose install to python module install

--- install_compose.sh
+++ install_compose.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-curl -Ls https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+#curl -Ls https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 
-chmod +x /usr/local/bin/docker-compose
+#chmod +x /usr/local/bin/docker-compose
+
+sudo apt-get install -qq python-pip
+
+sudo -H LC_ALL=C pip install docker-compose
 
